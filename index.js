dc99deb7d691d848ee3c57327e1b6f21383121a9
abelmcelroy <abel.h.mcelroy@gmail.com>
2023-10-11 16:41:25 -0400

    simplifying pokemon branch

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 import axios from "axios";
 
 async function pokemon() {
-  let currentRow;
   for (let i = 1; i <= 150; i++) {
-    // add a new row of sprites if current row is full
-    if ((i-1)%10 === 0) {
-      currentRow = document.createElement("div");
-      document.body.appendChild(currentRow);
-    }
-
-    // add a sprite to the current row
+    // creating an img tag
     const sprite = document.createElement("img");
-    currentRow.appendChild(sprite);
-
-    // finally, set the src for this sprite, once the API responds to us, whenever that happens
-
-    // Concurrent version
-    axios.get("https://pokeapi.co/api/v2/pokemon/" + i).then(response => {
-      sprite.src = response.data.sprites.front_default;
-    });
     
-    // Sequential version
+    // requesting the `i`-th pokemon's data
     const pokemonData = await axios.get("https://pokeapi.co/api/v2/pokemon/" + i);
+
+    // setting the src attribute to the sprite url even though the img tag isn't being attached to the DOM yet
     sprite.src = pokemonData.data.sprites.front_default;
+
+    document.body.appendChild(sprite);
   }
 }
 
