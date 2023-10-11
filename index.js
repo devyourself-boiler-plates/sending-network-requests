365d8125cabd0612ec9fe9c69b2b32386cae3854
abelmcelroy <abel.h.mcelroy@gmail.com>
2023-10-11 16:51:26 -0400

    correcting comments

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ async function pokemon() {
     // requesting the `i`-th pokemon's data
     const pokemonData = await axios.get("https://pokeapi.co/api/v2/pokemon/" + i);
 
-    // setting the src attribute to the sprite url even though the img tag isn't being attached to the DOM yet
+    // setting the src attribute to the sprite url
     sprite.src = pokemonData.data.sprites.front_default;
 
+    // appending the img tag to the DOM
     document.body.appendChild(sprite);
   }
 }
